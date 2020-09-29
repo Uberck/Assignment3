a0c5783218d3f1af5a4fe41ce7ab448ca05267ed
Christos Kokkalis <Uberck@gmail.com>
2020-09-28 20:28:53 -0400

    Add project files.

---
+++ Assignment3/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Assignment3.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="height: 283px">
+        &nbsp;
+            <asp:Label ID="lblTitle" runat="server" Text="Please enter a number less than 15:"></asp:Label>
+&nbsp;<br />
+&nbsp;&nbsp;
+            <br />
+            <asp:TextBox ID="txtInput" runat="server" Height="16px"></asp:TextBox>
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <asp:Button ID="btnCalculate" runat="server" OnClick="btnCalculate_Click" Text="Calculate Factorial" />
+            <br />
+            <br />
+            <asp:Label ID="lblAnswer" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Assignment3/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment3
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblTitle.Text = "Enter a number less than 15:";
+        }
+        // Logic for factorial button
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int factorial = 1;
+                // Convert input type to integer
+                int input = Convert.ToInt32(txtInput.Text);
+                if (input < 15)
+                {
+                    // For loop to calculate factorial
+                    for (int i = 1; i <= input; i++)
+                    {
+                        factorial = factorial * i;
+                    }
+                    // Print factorial in label
+                    lblAnswer.Text = "The factorial is " + factorial;
+                }
+                else
+                {
+                    // Popup for numbers entered greater than 15
+                    ClientScript.RegisterStartupScript(this.GetType(), "Alert", "alert('" + " The number entered must be less than 15" + "');", true);
+                }
+            }
+            catch (Exception)
+            {
+                // Runtime exception handling for invalid input
+                ClientScript.RegisterStartupScript(this.GetType(), "Alert", "alert('" + "Invalid Input, please try again." + "');", true);
+            }
+        }
+    }
+}
